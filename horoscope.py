7c9b338348bd38d282147d7222bbcfc5161c8d88
Georgina Melling <156523659+georginamelling@users.noreply.github.com>
2026-06-07 14:19:50 +0100

    horoscope added

--- horoscope.py
+++ horoscope.py
@@ -1,7 +1,15 @@
 import requests
 
 signs = requests.get("http://sandipbgt.com/theastrologer/api/sunsigns/").json()
+user_sign = ""
 
-print("Please select your star sign:")
+print("** STAR SIGNS **")
 for sign in signs:
-    print(f"  - {sign}")
+    print(f"  - {sign.title()}")
+    
+while user_sign not in signs:
+    user_sign = input("Please select your star sign: ").lower()
+    
+horoscope_json = requests.get(f"http://sandipbgt.com/theastrologer/api/horoscope/{user_sign}/today").json()
+
+print(horoscope_json["horoscope"])
