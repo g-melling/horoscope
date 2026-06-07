44efc96c23a0e11dd7247b5ccee571cb213915c0
Georgina Melling <156523659+georginamelling@users.noreply.github.com>
2026-06-07 14:01:27 +0100

    star signs added

--- horoscope.py
+++ horoscope.py
@@ -1,1 +1,7 @@
-print("hi")
+import requests
+
+signs = requests.get("http://sandipbgt.com/theastrologer/api/sunsigns/").json()
+
+print("Please select your star sign:")
+for sign in signs:
+    print(f"  - {sign}")
